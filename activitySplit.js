682974692de46ab79093c6a7cb805e415538e3cf
amcleod72 <andrew@mcleods.net>
2017-06-15 14:50:06 +1000

    first commit

--- activitySplit.js
+++ activitySplit.js
@@ -1,5 +1,12 @@
 <script runat="server" language="JavaScript">
 	Platform.Load("core","1");
 
-	Platform.Response.Redirect("https://amcleod72.github.io/routes/controlpath.json");
+	// Create a random number to emulate decisioning
+    var rand = Math.floor(Math.random() * 5);
+
+    if (rand == 1){
+        Platform.Response.Redirect("https://amcleod72.github.io/routes/controlpath.json");
+    } else {
+        Platform.Response.Redirect("https://amcleod72.github.io/routes/targetpath.json");
+    }
 </script>
